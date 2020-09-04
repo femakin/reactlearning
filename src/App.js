7e43f149d3167789c18a0aa21ea085a2cef09764
joshtom <olajidejoshua4real@gmail.com>
2020-09-04 15:07:59 +0100

    Update App component

--- src/App.js
+++ src/App.js
@@ -2,44 +2,41 @@ import React from 'react';
 import './index.css' 
 import Language from './Components/Language';
 
+const languageList = [
+  {
+    id: 1,
+    name: 'HTML & CSS',
+    image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg'
+  },
+  {
+    id: 2,
+    name: 'JavaScript',
+    image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg'
+  },
+  {
+    id: 3,
+    name: 'React',
+    image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg'
+  },
+  {
+    id: 4,
+    name: 'Ruby',
+    image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/ruby.svg'
+  },
+  {
+    id: 5,
+    name: 'Ruby on Rails',
+    image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/rails.svg'
+  },
+  {
+    id: 6,
+    name: 'Python',
+    image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/python.svg'
+  }
+];
+
 
 function App() {
-
-    const languageList = [
-      {
-        id: 1,
-        name: 'HTML & CSS',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg'
-      },
-      {
-        id: 2,
-        name: 'JavaScript',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg'
-      },
-      {
-        id: 3,
-        name: 'React',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg'
-      },
-      {
-        id: 4,
-        name: 'Ruby',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/ruby.svg'
-      },
-      {
-        id: 5,
-        name: 'Ruby on Rails',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/rails.svg'
-      },
-      {
-        id: 6,
-        name: 'Python',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/python.svg'
-      }
-    ];
-
-
-
     return (
       <div>
         <h1>List of Languages</h1>
@@ -55,8 +52,6 @@ function App() {
               />
               
             )
-            
-            
           })}
 
         </div>
